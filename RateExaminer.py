e691b0faa99ebf1d00b56643a030ecb1f0eac24f
Sadra Yavar <sadra.yavarzadeh@gmail.com>
2024-04-20 14:27:53 +0330

    add: json folder and session-based files add: json save and read methods

--- RateExaminer.py
+++ RateExaminer.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from datetime import datetime
 
 
@@ -23,3 +23,24 @@ class RateExaminer:
         self.log_file = os.path.join(log_folder_path, f"{current_time}.log")
         if not os.path.exists(log_folder_path):
             os.makedirs(log_folder_path)
+
+        # json folder and files
+        json_folder_path = os.path.join(self.current_path, "json")
+        self.json_file = os.path.join(json_folder_path, f"{current_time}.json")
+        if not os.path.exists(json_folder_path):
+            os.makedirs(json_folder_path)
+        with open(self.json_file, "w") as file:
+            json.dump({"data": []}, file)
+
+    """################################################################################# saving & log
+    """  #################################################################################
+
+    def save_json(self, dict):
+        file_content = self.read_json()
+        with open(self.json_file, "w") as f:
+            final_data = {"data": file_content["data"] + [dict]}
+            json.dump(final_data, f, indent=4)
+
+    def read_json(self):
+        with open(self.json_file, "r") as f:
+            return json.load(f)
